014eafd44686ac303d76563c430f75d27ece86ee
Shubham020303 <72245331+Shubham020303@users.noreply.github.com>
2023-07-30 09:41:17 +0000

    google auth done

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -29,7 +29,7 @@ const AuthProviders = () => {
 		return (
 			<div>
 				{Object.values(providers).map((provider: Provider, index) => (
-					<button key={index}>{provider.id}</button>
+					<button key={index} onClick={() => { signIn(provider?.id) }}>{provider.id}</button>
 				))}
 			</div>
 		)
